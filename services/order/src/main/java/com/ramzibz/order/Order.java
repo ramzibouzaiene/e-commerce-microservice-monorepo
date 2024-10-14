ae6805fe67c90e8db5abcafcdc7028e0707e5133
Ramzi BOUZAIENE <ramzi.bouzaiene@proxym-it.com>
2024-10-14 15:28:44 +0100

    adding and configure the order service

---
+++ services/order/src/main/java/com/ramzibz/order/Order.java
@@ -0,0 +1,51 @@
+package com.ramzibz.order;
+
+import com.ramzibz.orderLine.OrderLine;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "customer_order")
+public class Order {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String reference;
+    private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+    private String customerId;
+    @OneToMany(mappedBy = "order")
+    private List<OrderLine> orderLines;
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedDate;
+}
--- services/product/src/main/java/com/ramzibz/product/product/ProductController.java
+++ services/product/src/main/java/com/ramzibz/product/product/ProductController.java
@@ -2,7 +2,6 @@ package com.ramzibz.product.product;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,28 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public ResponseEntity<Integer> creeaetProduct(@RequestBody @Valid ProductRequest productRequest){
+    public ResponseEntity<Integer> creeaetProduct(@RequestBody @Valid ProductRequest productRequest) {
         return ResponseEntity.ok(productService.createProduct(productRequest));
     }
 
     @PostMapping("/purchase")
-    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(@RequestBody List<ProductPurchaseRequest> requests){
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(@RequestBody List<ProductPurchaseRequest> requests) {
         return ResponseEntity.ok(productService.purchaseProducts(requests));
     }
 
     @GetMapping("/{product-id}")
-    public ResponseEntity<ProductResponse> findById(@PathVariable("product-id") Integer productId){
+    public ResponseEntity<ProductResponse> findById(@PathVariable("product-id") Integer productId) {
         return ResponseEntity.ok(productService.findProductById(productId));
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductResponse>> getAllProducts(){
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
         return ResponseEntity.ok(productService.getAllProducts());
     }
 }
